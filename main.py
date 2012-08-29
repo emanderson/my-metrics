2075f0e3d8ba4b71dc0a25df5b4814968d0c0da0
Emily Anderson <emily@eandersons.net>
2012-08-29 09:02:35 -0700

    Use path matcher to include custom data in the page

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from pyramid.view import view_config
 
 @view_config(route_name='hello', renderer='test.html')
 def hello_world(request):
-    return {'hello': 'world'}
+    return {'hello': request.matchdict['name']}
 
 if __name__ == '__main__':
     config = Configurator()
     config.add_renderer(name='.html', factory='renderers.jinja2_renderer.Jinja2Renderer')
     config.add_route('hello', '/hello/{name}')
-    #config.add_view(hello_world, route_name='hello', renderer='test.html')
     config.scan()
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8084, app)
