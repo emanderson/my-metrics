fd092f9b555b04ab3e2e38bfccd32a3b0b345763
Emily Anderson <emily@eandersons.net>
2012-09-06 15:11:56 -0700

    Pages to list and add food tags

--- views/food_management.py
+++ views/food_management.py
@@ -2,12 +2,34 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 
 from tables.food import Food
+from tables.food_tag import FoodTag
 from tables import Session
 
 @view_config(route_name='food-list', renderer='food_list.html')
-def food_entry_list(request):
+def food_list(request):
     session = Session()
     foods = session.query(Food).order_by(Food.name)
     session.close()
     return {'title': 'Foods', 'foods': foods}
 
+@view_config(route_name='food-tag-list', renderer='food_tag_list.html')
+def food_tag_list(request):
+    session = Session()
+    food_tags = session.query(FoodTag).order_by(FoodTag.name)
+    session.close()
+    return {'title': 'Food Tags', 'food_tags': food_tags}
+
+@view_config(route_name='food-tag-add-form', renderer='food_tag_add_form.html')
+def food_tag_add_form(request):
+    return {'title': 'Add a Food Tag'}
+
+@view_config(route_name='food-tag-add')
+def food_tag_add(request):
+    name = request.params['name']
+    
+    session = Session()
+    new_tag = FoodTag(name)
+    session.add(new_tag)
+    session.commit()
+    
+    return HTTPFound('/food_tag/list')
